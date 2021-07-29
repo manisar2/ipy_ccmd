da0cd849c5c9be63a7ac2647498406b941806e10
manisar2 <sareen.mani11@gmail.com>
2021-07-28 18:30:29 -0600

    Removed code cell

--- src/ipy_ccmd/__init__.py
+++ src/ipy_ccmd/__init__.py
@@ -1,6 +1,5 @@
 # Show markdown in IPython output from within a code cell (selectively)
 # For details see https://randompearls.com/science-and-technology/information-technology/coding-and-development-reference-and-tools/show-markdown-within-code-cells-jupyter-and-vs-code-interactive-python/
-# %%
 import sys
 from enum import Enum
 from forbiddenfruit import curse
